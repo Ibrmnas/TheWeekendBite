83ffef6520dd336f158e473a66c66d3697c23d26
Ibrahim Nasser <106866448+Ibrmnas@users.noreply.github.com>
2025-10-19 12:46:15 +0200

    Update backend.js

--- assets/js/backend.js
+++ assets/js/backend.js
@@ -1,2 +1,2 @@
 // Paste your Apps Script Web App URL between the quotes:
-window.WB_ENDPOINT = "https://script.google.com/macros/s/AKfycbyKURmYxasBjMihFVzU-qFjG71Mt2EGkIrhlnUVj8sZmBltJo8qOhhuU75blLzUzP8bDg/exec"; // e.g., https://script.google.com/macros/s/AKfycbx.../exec
+window.WB_ENDPOINT = "https://script.google.com/macros/s/AKfycby6l2MiCfRn-bm-1xIT6t3ruzJasGoMWvkbAl9xOYZajaJvE-aDCiuHEZsMWHklfhYmbA/exec"; // e.g., https://script.google.com/macros/s/AKfycbx.../exec
